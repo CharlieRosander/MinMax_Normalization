a8b4275d3b2ff7f074a454a5a2b803ed451cc779
CharlieRosander <112691863+CharlieRosander@users.noreply.github.com>
2023-02-13 15:50:19 +0100

    asd

--- test3.py
+++ test3.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+
 
 class Data:
     value = None
@@ -16,7 +16,8 @@ class Data:
     @classmethod
     def data_preprocess(cls, dataframe, value):
         cls.value = value
-        print(f"Number of NaN values:\n{dataframe.isnull().sum()}")
+        print(f"Columns containing NaN-values:\n{dataframe.isnull().sum()[dataframe.isnull().sum() > 0]}")
+
         if value == "show":
             print(dataframe.isnull().sum())
 
@@ -25,28 +26,25 @@ class Data:
 
         elif value == 0:
             dataframe.fillna(0, inplace=True)
-            print(f"Changed NaN values to 0.\n{dataframe.isnull().sum()}")
+            print(f"\nChanged all NaN values to 0.\n{dataframe.isnull().sum}")
 
     @classmethod
     def set_columns(cls, dataframe):
-        for col in dataframe.columns[1:]:
-            if all(isinstance(x, (int, float)) for x in dataframe[col]):
-                if not dataframe[col].dtype in ['bool']:
-                    Data.columns_to_normalize.append(col)
+        if "index" or "idx" or "ix" in dataframe.columns:
+            for col in dataframe.columns[1:]:
+                if all(isinstance(x, (int, float)) for x in dataframe[col]):
+                    if not dataframe[col].dtype in ['bool']:
+                        Data.columns_to_normalize.append(col)
+        else:
+            for col in dataframe.columns:
+                if all(isinstance(x, (int, float)) for x in dataframe[col]):
+                    if not dataframe[col].dtype in ['bool']:
+                        Data.columns_to_normalize.append(col)
+
+        return print(f"These are the columns that will be normalized: \n{Data.columns_to_normalize}")
 
     @staticmethod
     def min_max_norm(dataframe):
-        for column in dataframe[Data.columns_to_normalize]:
-            col_min = dataframe[column].min()
-            col_max = dataframe[column].max()
-            dataframe[column + "_normalized"] = (dataframe[column] - col_min) / (col_max - col_min)
-
-    @staticmethod
-    def min_max_norm_replace(dataframe):
-        for column in dataframe:
-            if "_normalized" in column:
-                dataframe.drop(column, axis=1, inplace=True)
-
         for column in dataframe[Data.columns_to_normalize]:
             col_min = dataframe[column].min()
             col_max = dataframe[column].max()
@@ -63,7 +61,5 @@ df_norm = csv.read_csv()
 
 Data.data_preprocess(df_norm, 0)
 Data.set_columns(df_norm)
-print(Data.columns_to_normalize)
-Data.min_max_norm_replace(df_norm)
+Data.min_max_norm(df_norm)
 Data.save_to_csv(df_norm, "Normaliserad_csv")
-print(df_norm)
