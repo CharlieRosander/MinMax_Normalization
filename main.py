4bcf0dfca5a0f3ea78caef041f9d8602b6e556ac
CharlieRosander <112691863+CharlieRosander@users.noreply.github.com>
2023-02-16 09:10:09 +0100

    asd

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ class Data:
 
     @staticmethod
     def read_csv():
-        csv_dataframe = pd.read_csv(csv.csv_file)
-        return csv_dataframe
+        try:
+            csv_dataframe = pd.read_csv(csv.csv_file)
+            return csv_dataframe
+        except ValueError:
+            print(ValueError, "The CSV-file is empty.")
+            exit()
 
     @classmethod
     def data_preprocess(cls, dataframe, value):
@@ -26,7 +30,7 @@ class Data:
 
         elif value == 0:
             dataframe.fillna(0, inplace=True)
-            print(f"\nChanged all NaN values to 0.\n{dataframe.isnull().sum}")
+            print(f"\nChanged all NaN values to 0.\n{dataframe.isnull().sum()}")
 
     @classmethod
     def set_columns(cls, dataframe):
@@ -48,18 +52,20 @@ class Data:
         for column in dataframe[Data.columns_to_normalize]:
             col_min = dataframe[column].min()
             col_max = dataframe[column].max()
-            dataframe[column] = (dataframe[column] - col_min) / (col_max - col_min)
+            dataframe[column] = ((x for x in dataframe[column]) - col_min) / (col_max - col_min)
 
     @staticmethod
-    def save_to_csv(dataframe, file_name):
+    def save_to_csv(dataframe):
+        file_name = input("\nEnter the name for the CSV file: ")
         dataframe.to_csv(file_name + ".csv", index=False)
 
 
-csv = Data("Automobile_data.csv")
-df = csv.read_csv()
-df_norm = csv.read_csv()
+if __name__ == "__main__":
+    csv = Data("Automobile_data.csv")
+    df = csv.read_csv()
+    df_norm = csv.read_csv()
 
-Data.data_preprocess(df_norm, 0)
-Data.set_columns(df_norm)
-Data.min_max_norm(df_norm)
-Data.save_to_csv(df_norm, "Normaliserad_csv")
+    Data.data_preprocess(df_norm, "drop")
+    Data.set_columns(df_norm)
+    Data.min_max_norm(df_norm)
+    Data.save_to_csv(df_norm)
